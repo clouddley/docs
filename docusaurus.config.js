39389d0fa408fe9ef2103fbd26ffad6687eeda7b
Obinna Odirionye <odirionye@gmail.com>
2022-11-19 23:37:25 +0400

    docs: update docs

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -39,7 +39,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/clouddley/docs/blog/main/',
+            'https://github.com/clouddley/docs/blob/main/',
         },
         blog: false,
         theme: {
